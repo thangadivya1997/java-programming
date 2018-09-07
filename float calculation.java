ab73a2679a95ea0cca2523689e9ff5e8c649d074
thangadivya1997 <43057105+thangadivya1997@users.noreply.github.com>
2018-09-07 11:35:13 +0530

    Add files via upload

---
+++ float calculation.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+   float p,q,r,s,t,u;
+    p = 2.5f;
+    q = 5.5f;
+    r = p+q;
+    s = p-q;
+    t = p*q;
+    u = p/q;
+    System.out.println("addition of two numbers"+r);
+    System.out.println("subtraction of two numbers "+s);
+    System.out.println("product of two numbers"+t);
+    System.out.println("division of two numbers"+u);
+  }
+}
