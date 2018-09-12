91f7e7d48a56d8bc1a6cc12957d235f71f7c5239
thangadivya1997 <43057105+thangadivya1997@users.noreply.github.com>
2018-09-12 15:37:40 +0530

    Add files via upload

---
+++ hit,flop calculation.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main (String[]args){
+  int dayslntheatre = 50;
+      if(dayslntheatre<=50)
+        System.out.println("Box Office Hit");
+    else
+       System.out.println("Box Office Flop");
+  }
+}
