d5186e29c938666152e1ceb399eddbed590e2a1e
thangadivya1997 <43057105+thangadivya1997@users.noreply.github.com>
2018-09-12 15:15:32 +0530

    Add files via upload

---
+++ two number.java
@@ -0,0 +1,37 @@
+class Main {
+  public static void main(String[] args) {
+     int a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p;
+      a = 4;
+      b = 2;
+      c = a+b;
+      d = a-b;
+      e = a*b;
+      f = b/a;
+      g = b%a;
+      h = b++;
+      i = b--;
+      j = a&b;
+      k = a|b;
+      l = a^b;
+      m = ~a;
+      n = a<<2;
+      o = a>>2;
+      p = a>>>2;
+     
+      System.out.println("output is"+c); 
+      System.out.println("output is"+d); 
+      System.out.println("output is"+e); 
+      System.out.println("output is"+f); 
+      System.out.println("output is"+g); 
+      System.out.println("output is"+h); 
+      System.out.println("output is"+i);
+      System.out.println("output is"+j); 
+      System.out.println("output is"+k); 
+      System.out.println("output is"+l); 
+      System.out.println("output is"+m);
+      System.out.println("output is"+n);
+      System.out.println("output is"+o); 
+      System.out.println("output is"+p); 
+      
+     }
+}
