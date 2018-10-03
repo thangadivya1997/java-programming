ea0861ca52bb5ba5d4c8864550e6995b62ae7064
thangadivya1997 <43057105+thangadivya1997@users.noreply.github.com>
2018-10-03 12:30:34 +0530

    Add files via upload

---
+++ negative or positive or zero.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    int num = 75;
+    if (num > 0)
+    {
+      System.out.println(num+"is a positive num");
+    }
+    else if (num  <0)
+    {
+      System.out.println(num+"is a negative num");
+    }
+    else if (num  <= 0)
+    {
+      System.out.println(num+"is a zero");
+    }
+     else
+    {
+     System.out.println(num+"is neither positive or negative or zero"); 
+    }
+ }
+}
