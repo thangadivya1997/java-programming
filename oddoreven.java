954374ba06dc8a40542403030ffb02631f28cc9c
thangadivya1997 <43057105+thangadivya1997@users.noreply.github.com>
2018-10-04 12:20:07 +0530

    Update oddoreven.java

--- oddoreven.java
+++ oddoreven.java
@@ -4,8 +4,8 @@ class oddoreven {
   Scanner reader = new Scanner(System.in);
   int num=reader.nextInt();
   if(num% 2 == 0 )
-  System.out.println("even");
+  System.out.println("Even");
   else
-  System.out.println("odd");
+  System.out.println("Odd");
   }
 }
