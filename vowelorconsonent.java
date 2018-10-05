2d85446f6c31ceb85220581ae33cb4854ad735cf
thangadivya1997 <43057105+thangadivya1997@users.noreply.github.com>
2018-10-05 11:40:57 +0530

    Add files via upload

---
+++ vowelorconsonent.java
@@ -0,0 +1,16 @@
+class Main {
+ public static void main(String[] args) {
+ char ch = 'z';
+ switch (ch) {
+ case 'a':
+ case 'e':
+ case 'i':
+ case 'o':
+ case 'u':
+ System.out.println(ch + " is Vowel");
+ break;
+ default:
+ System.out.println(ch + " is Consonant");
+  }
+ }
+}
