2b1f7d4dd4028bc61ce9ab7d91e88070038e3bbe
thangadivya1997 <43057105+thangadivya1997@users.noreply.github.com>
2019-02-12 14:11:03 +0530

    Add files via upload

---
+++ n number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main {
+ public static void main(String[] args) {
+ Scanner ka=new Scanner(System.in);
+ int N;
+ System.out.println("enter the n values");N=ka.nextInt();
+ int i = 1;
+ while (i <= N) {
+ System.out.println("Hello World");
+  i++;
+ }
+}
+}
