fa0d5733581d393e9ab9bd6156054f1b615e2368
thangadivya1997 <43057105+thangadivya1997@users.noreply.github.com>
2019-02-12 10:45:45 +0530

    Add files via upload

---
+++ paindrome.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    int a = 121,b = 121;
+    if(a==b)
+    System.out.println("a is a palindrome");
+    else
+    System.out.println("a is not a palindrome");
+
+  }
+}
